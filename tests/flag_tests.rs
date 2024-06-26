aa0eeed8e0a9984726245b3c445da0732457e820
PootisHunter <98407040+PootisHunter@users.noreply.github.com>
2024-06-26 12:55:20 +0300

    Add files via upload

---
+++ tests/flag_tests.rs
@@ -0,0 +1,25 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use autograder::flag_generator;
+    #[test]
+    fn test_new() {
+        assert!(generate_uuid().is_ok());
+    }
+    #[test]
+    fn test_compare_hmac() {
+        let id = Uuid::now_v7();
+        let secret = "Work".to_string();
+        let taskid = "task1".to_string();
+        let secret2 = "Work".to_string();
+        let taskid2 = "task1".to_string();
+        let hash = generate_hmac(id, secret, taskid).expect("error");
+        print!("{}", hash);
+        assert!(compare_hmac(hash, id, secret2, taskid2).expect("should work"))
+    }
+
+    #[test]
+    fn test_rand() {
+        assert!(generate_flag32().is_ok());
+    }
+}

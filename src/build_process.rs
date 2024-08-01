bcc8937496127656705b429738071e5090dfc662
Kemppis3 <jonkempp21@student.oulu.fi>
2024-08-01 13:19:58 +0300

    Added flag_types back to subtasks in config and fixed flag identification functions in build_process

--- src/build_process.rs
+++ src/build_process.rs
@@ -1,15 +1,15 @@
 //File for initializing the build process
 use std::error::Error;
 use crate::config::CourseConfiguration;
-use crate::flag_gen;
+use crate::flag_generator;
 
 pub fn identify_flag_types_for_task(course_config: CourseConfiguration, week_number: usize, task_id: String) {
     for week in course_config.weeks.iter() {
         if week.number == week_number {
             for task in week.tasks.iter() {
                 if task.id == task_id {
-                    if task.subtasks.is_some() {
-                        for subtask in task.subtasks.iter() {
+                    if let Some(subtasks) = &task.subtasks {
+                        for subtask in subtasks.iter() {
                             for flag_type in task.flag_types.iter() {
                                 if flag_type.id == subtask.id {
                                     subtask.flag_type = flag_type;
@@ -28,8 +28,8 @@ pub fn identify_flag_types_for_week(course_config: CourseConfiguration, week_num
     for week in course_config.weeks.iter() {
         if week.number == week_number {
             for task in week.tasks.iter() {
-                if task.subtasks.is_some() {
-                    for subtask in task.subtasks.iter() {
+                if let Some(subtasks) = &task.subtasks {
+                    for subtask in subtasks.iter() {
                         for flag_type in task.flag_types.iter() {
                             if flag_type.id == subtask.id {
                                 subtask.flag_type = flag_type;
@@ -44,16 +44,18 @@ pub fn identify_flag_types_for_week(course_config: CourseConfiguration, week_num
 }
 
 pub fn identify_all_flag_types(course_config: CourseConfiguration) {
-        for (i,week) in course_config.weeks.iter().enumerate() {
-            if week.tasks[i].subtasks.is_some() {
-            for (j, subtask) in week.tasks[i].subtasks.iter().enumerate() {
-                for flag_type in week.tasks[i].flag_types.iter() {
-                    if flag_type.id == subtask[j].id {
-                        subtask[j].flag_type = flag_type;
-                        break;
+        for week in course_config.weeks.iter() {
+            for task in week.tasks.iter() {
+                if let Some(subtasks) = &task.subtasks {
+                    for subtask in subtasks.iter() {
+                        for flag_type in task.flag_types.iter() {
+                            if flag_type.id == subtask.id {
+                                subtask.flag_type = flag_type;
+                                break;
+                        }
                     }
+                }
             }
         }
     } 
-}
 }

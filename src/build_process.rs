be3a2e8a61c8181359e985694c8fa5490e333c29
PootisHunter <reetu630@gmail.com>
2024-08-07 09:40:54 +0300

    korjailua3

--- src/build_process.rs
+++ src/build_process.rs
@@ -1,11 +1,6 @@
 //File for initializing the build process
 use crate::config::CourseConfiguration;
-<<<<<<< HEAD
-use crate::flag_gen;
-use std::error::Error;
-=======
 use crate::flag_generator;
->>>>>>> 8adf09037a0c502713d8069b84007c541e15c2fb
 
 pub fn identify_flag_types_for_task(
     course_config: CourseConfiguration,
@@ -52,18 +47,18 @@ pub fn identify_flag_types_for_week(course_config: CourseConfiguration, week_num
 }
 
 pub fn identify_all_flag_types(course_config: CourseConfiguration) {
-        for week in course_config.weeks.iter() {
-            for task in week.tasks.iter() {
-                if let Some(subtasks) = &task.subtasks {
-                    for subtask in subtasks.iter() {
-                        for flag_type in task.flag_types.iter() {
-                            if flag_type.id == subtask.id {
-                                subtask.flag_type = flag_type;
-                                break;
+    for week in course_config.weeks.iter() {
+        for task in week.tasks.iter() {
+            if let Some(subtasks) = &task.subtasks {
+                for subtask in subtasks.iter() {
+                    for flag_type in task.flag_types.iter() {
+                        if flag_type.id == subtask.id {
+                            subtask.flag_type = flag_type;
+                            break;
                         }
                     }
                 }
             }
         }
-    } 
+    }
 }

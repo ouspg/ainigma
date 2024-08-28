e11a8cf3478f262ebb88a286b459215f817f8374
Niklas Saari <niklas.saari@tutanota.com>
2024-08-28 12:53:09 +0300

    Add very simple sample task

---
+++ samples/tasks/simple_shell/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Exit on fail
+set -e 
+
+echo "Building very simple bash script..."
+
+# Create the script
+cat << EOF > secret.sh
+#!/bin/bash
+
+# The flag is now hardcoded in the script
+FLAG="$FLAG"
+
+echo "The flag is: \$FLAG"
+EOF
+
+# Make the inner script executable
+chmod +x secret.sh
+
+echo "Task script created successfully."
+

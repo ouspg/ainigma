7b3fbe82977dfa1d2a12893eac50ce2f6af62cb7
PootisHunter <reetu630@gmail.com>
2024-08-30 11:15:31 +0300

    conflict resolve

--- src/bin/autograder_cli.rs
+++ src/bin/autograder_cli.rs
@@ -104,9 +104,9 @@ fn moodle_build(
             &week,
             &task.as_ref().unwrap()
         );
-        let mut result = read_check_toml(path.into_os_string().as_os_str())?;
+        let result = read_check_toml(path.into_os_string().as_os_str())?;
         let uuid = Uuid::now_v7();
-        build_task(&mut result, task.unwrap(), uuid)
+        build_task(result, task.unwrap(), uuid)
         //TODO: Generating moodle tasks
     } else {
         println!("Generating moodle task for week {}", &week);

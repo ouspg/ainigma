e3e68c9679ef0b24d01fe2ced0e79641c9410b41
PootisHunter <reetu630@gmail.com>
2024-08-30 14:34:01 +0300

    build imp

--- src/build_process.rs
+++ src/build_process.rs
@@ -1,9 +1,9 @@
 use core::str;
-use std::env;
+use std::collections::HashMap;
 use uuid::Uuid;
 
 use crate::config::{BuildConfig, CourseConfiguration, Task};
-use crate::flag_generator::{self, Flag};
+use crate::flag_generator::{self};
 
 /*
 struct GenerationOutputs {
@@ -17,8 +17,8 @@ fn create_flag_id_pairs_by_task<'a>(
     task_config: &'a Task,
     course_config: &'a CourseConfiguration,
     uuid: Uuid,
-) -> Vec<(&'a str, Flag)> {
-    let mut flags = Vec::with_capacity(3);
+) -> HashMap<String, String> {
+    let mut flags = HashMap::with_capacity(3);
     for stage in &task_config.stages {
         // let task_id = part.id.clone();
         let id: &str;
@@ -27,58 +27,44 @@ fn create_flag_id_pairs_by_task<'a>(
         } else {
             id = &task_config.id
         }
-        flags.push(match stage.flag.kind.as_str() {
-            "user_derived" => (
-                id,
-                flag_generator::Flag::user_flag(
+        match stage.flag.kind.as_str() {
+            "user_derived" => {
+                let flag_value = flag_generator::Flag::user_flag(
                     id.into(),
                     course_config.flag_types.user_derived.algorithm.clone(),
                     course_config.flag_types.user_derived.secret.clone(),
                     id.into(),
                     uuid,
-                ),
-            ),
-            "pure_random" => (
-                id,
-                flag_generator::Flag::random_flag(
+                )
+                .flag_string();
+                let flag_key = format!("flag_user_derived_{}", id.to_string());
+                flags.insert(flag_key, flag_value);
+            }
+            "pure_random" => {
+                let flag_value = flag_generator::Flag::random_flag(
                     id.into(),
                     course_config.flag_types.pure_random.length,
-                ),
-            ),
-            "rng_seed" => (
-                id,
-                flag_generator::Flag::user_seed_flag(
+                )
+                .flag_string();
+                let flag_key = format!("flag_pure_random_{}", id.to_string());
+                flags.insert(flag_key, flag_value);
+            }
+            "rng_seed" => {
+                let flag_value = flag_generator::Flag::user_seed_flag(
                     id.into(),
                     course_config.flag_types.user_derived.algorithm.clone(),
                     course_config.flag_types.user_derived.secret.clone(),
                     id.into(),
                     uuid,
-                ),
-            ),
+                )
+                .flag_string();
+                let flag_key = format!("flag_user_seed_{}", id.to_string());
+                flags.insert(flag_key, flag_value);
+            }
             _ => panic!("Invalid flag type"),
-        });
+        };
     }
     flags
-}
-
-fn set_flags_to_env_variables(flags: &[(&str, Flag)]) {
-    match flags.len() {
-        0 => {
-            // TODO: Add error handling
-            panic!("Empty Flag vector provided")
-        }
-        1 => {
-            let env_var_flag = flag_generator::Flag::flag_string(&flags[0].1);
-            std::env::set_var("FLAG", env_var_flag);
-        }
-        _ => {
-            for (i, (_id, flag)) in flags.iter().enumerate() {
-                let env_var_flag = flag_generator::Flag::flag_string(flag);
-                let env_var_name = format!("FLAG_{}", i + 1);
-                std::env::set_var(env_var_name, env_var_flag);
-            }
-        }
-    }
 }
 
 #[allow(dead_code)]
@@ -110,13 +96,14 @@ pub fn build_task(
         .get_task_by_id(&task_id)
         .unwrap_or_else(|| panic!("Task with id {} not found in course configuration", task_id));
     let id_flag_pairs = create_flag_id_pairs_by_task(task_config, course_config, uuid);
-    set_flags_to_env_variables(&id_flag_pairs);
 
-    env::set_current_dir(&task_config.build.directory)
-        .expect("failed to locate resource directory");
+    //env::set_current_dir(&task_config.build.directory)
+    //    .expect("failed to locate resource directory");
 
-    let output = std::process::Command::new("sh")
+    let output = std::process::Command::new("powershell")
         .arg(task_config.build.entrypoint.clone())
+        .envs(id_flag_pairs)
+        .current_dir(&task_config.build.directory)
         .output()
         .expect("Failed to compile task");
 
